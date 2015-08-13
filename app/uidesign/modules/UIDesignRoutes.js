a0bf59130a51361385e7323caeb213ee45e868ab
Logan Bernard <bernard.logan4@gmail.com>
2015-08-13 16:36:00 -0400

    [uidesign] minor cleanup

--- app/uidesign/modules/UIDesignRoutes.js
+++ app/uidesign/modules/UIDesignRoutes.js
@@ -5,14 +5,8 @@ var content = require('./UIDesignContent');
 
 /**
  * @desc
- ** Routing is organized like so:
- **
- ** state - top level nav (navbar)
- ** section - component inside of state
- ** subsection - component inside of section
- **
- ** e.g. state.section.subsection -> 
- **      marketing.colors.primary
+ * Used for easy access to the content config file,
+ * using Angular's dependency injection.
  *
  * @ngInject
  */
