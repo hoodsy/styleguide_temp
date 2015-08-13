4e56b4efdf851de279f5cc1902cfbdf952ebe208
Logan Bernard <bernard.logan4@gmail.com>
2015-08-13 16:37:20 -0400

    [sections] refactor into functions

--- app/components/section/SectionService.js
+++ app/components/section/SectionService.js
@@ -9,50 +9,6 @@ function SectionService($filter) {
 
   var service = {};
 
-  /**
-   * @description 
-   * Creates filepaths for each subsection's content, primarily
-   * .html templates and markdown files.
-   *
-   * @param path {string}
-   * Path to files in a given section (e.g. uidesign/components/inputs)
-   * @param title {string}
-   * Formatted subsection title (e.g. Gray Scale -> grayscale)
-   * @param subsection {object}
-   * Subsection object - TODO provide a more specific argument
-   *
-   * @returns contentList {array}
-   * An array of content objects. Each content object has a tab name,
-   * assets filepath, and description filepath.
-  **/
-  service.createContent = function (path, title, subsection) {
-    // title = $filter('toSubstate')(title);
-    var titlePath = path + title + '/';
-    var contentList = [];
-
-    for (var i = 0; i < subsection.tabs.length; i++) {
-      var tab = subsection.tabs[i].toLowerCase();
-
-      var description;
-      if (subsection.tabs[i] === 'API' && subsection.component) {
-        description = 'dgeni/' + subsection.component;
-      }
-      else {
-        description = 'markdown/' + titlePath + tab + '.html'
-      }
-      var assets;
-      if (subsection.shareExample) assets = titlePath + 'example.html';
-      else assets = titlePath + tab + '.html';
-
-      var content = {
-        tab: subsection.tabs[i],
-        assets: assets,
-        description: description
-      };
-      contentList.push(content);
-    }
-    return contentList;
-  }
 
   /**
    * @description 
@@ -72,20 +28,96 @@ function SectionService($filter) {
 
     angular.forEach(sections, function(routes){
       var sectionTitle = $filter('toSubstate')(routes.section);
-      var path = section + '/components/' + sectionTitle + '/';
-      filledSections[sectionTitle] = {};
-      filledSections[sectionTitle].title = sectionTitle;
-
-      angular.forEach(routes.subsections, function(subsection){
-        var subsectionTitle = $filter('toSubstate')(subsection.title);
-        filledSections[sectionTitle][subsectionTitle] = {};
-        filledSections[sectionTitle][subsectionTitle].title = subsection.title;
-        filledSections[sectionTitle][subsectionTitle].tabs = subsection.tabs;
-        filledSections[sectionTitle][subsectionTitle].content = service.createContent(path, subsectionTitle, subsection);
-      });
+      var config = {
+        section: sectionTitle, 
+        path: section + '/components/' + sectionTitle + '/'
+      };
+      filledSections[sectionTitle] = attachSection(config, routes);
     });
+
     return filledSections;
   }
+
+  /**
+   * attachContent Helpers
+  **/
+
+  // Attach subsection content paths to section
+  function attachSection (config, routes) {
+    var sectionContent = attachSubsections(config.path, routes, {});
+    sectionContent.title = config.section;
+    return sectionContent;
+  }
+
+  // Attach subsection content paths to object
+  function attachSubsections (path, routes, sectionContent) {
+
+    angular.forEach(routes.subsections, function(subsection){
+      var subsectionTitle = $filter('toSubstate')(subsection.title);
+      sectionContent[subsectionTitle] = {
+        title: subsection.title,
+        tabs: subsection.tabs,
+        content: createContent(path, subsectionTitle, subsection)
+      };
+    });
+
+    return sectionContent;
+  }
+
+  /**
+   * @description 
+   * Creates filepaths for each subsection's content, primarily
+   * .html templates and markdown files.
+   *
+   * @param path {string}
+   * Path to files in a given section (e.g. uidesign/components/inputs)
+   * @param title {string}
+   * Formatted subsection title (e.g. Gray Scale -> grayscale)
+   * @param subsection {object}
+   * Subsection object - TODO provide a more specific argument
+   *
+   * @returns contentList {array}
+   * An array of content objects. Each content object has a tab name,
+   * assets filepath, and description filepath.
+  **/
+  function createContent (path, title, subsection) {
+    var titlePath = path + title + '/';
+    var contentList = [];
+
+    for (var i = 0; i < subsection.tabs.length; i++) {
+      contentList.push(getContent(subsection.tabs[i], titlePath, subsection));
+    }
+
+    return contentList;
+  }
+
+  /**
+   * createContent Helpers
+  **/
+
+  // Return content object
+  function getContent (tab, path, subsection) {
+    return {
+      tab: tab,
+      assets: getAssets(tab.toLowerCase(), path, subsection.shareExample),
+      description: getDescription(tab.toLowerCase(), path, subsection.component)
+    };
+  }
+
+  // Build description (markdown) path
+  function getDescription (tab, path, component) {
+    return (tab === 'api' && component)
+      ? 'dgeni/' + component
+      : 'markdown/' + path + tab + '.html';
+  }
+
+  // Build assets (html) path
+  function getAssets (tab, path, shareExample) {
+    return (shareExample)
+      ? path + 'example.html'
+      : path + tab + '.html';
+  }
+
 
   return service;
 
