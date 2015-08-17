2b5a6ef9baed3d89278b17eb32e95361520782e1
Logan Bernard <bernard.logan4@gmail.com>
2015-08-17 10:43:34 -0400

    [gulp] refactor generate task

--- gulp/tasks/generate.js
+++ gulp/tasks/generate.js
@@ -8,9 +8,9 @@ var mkdirp = require('mkdirp');
 var config = require('../config');
 
 // Content
-var appPath = '../../app/';
+var appPath   = '../../app/';
+var uidesign  = require(appPath+'uidesign/modules/UIDesignContent');
 var marketing = require(appPath+'marketing/modules/MarketingContent');
-var uidesign = require(appPath+'uidesign/modules/UIDesignContent');
 // var guidelines = require(appPath+'guidelines/modules/GuidelinesContent');
 
 gulp.task('generate', function() {
@@ -18,34 +18,51 @@ gulp.task('generate', function() {
 	var states = [marketing, uidesign];
 	states.forEach(function(state) {
 
-		if (state.state === 'Marketing') var shareExample = false;
-		else var shareExample = true;
-
+		var shareExample = shouldShareExample(state.state);
 		state.sections.forEach(function(section) {
-			var statePath = 'app/'+state.state+'/components/';
-
+			
+			var statePath = 'app/' + state.state + '/components/';
 			section.subsections.forEach(function(subsection) {
-				// Make section directories if they don't exist
-				var formattedTitle = subsection.title.replace(' ', '').toLowerCase();
-				var formattedSection = section.section.replace(' ', '').toLowerCase();
-				var path = statePath + formattedSection + '/' + formattedTitle;
-				
-				mkdirp(path, function (err) {
-					if (err) console.log(err);
-					else {
-						// Create shared html example template
-						if (shareExample) touch(path + '/example.html');
-
-						// Create markdown / html files for each tab
-						subsection.tabs.forEach(function(tab) {					
-							tab = tab.toLowerCase();
-							if (tab != 'api') touch(path + '/' + tab + '.md');
-							if (!shareExample) touch(path + '/' + tab + '.html');
-						});
-					}
-				});
+				var sectionTitle    = toStateFormat(section.section);
+				var subsectionTitle = toStateFormat(subsection.title);
+				var path = statePath + sectionTitle + '/' + subsectionTitle;
+				scaffoldSubsection(path, subsection.tabs, shareExample);				
 			});
 		});
 	})
+	
+	/**
+	 * Generate task helpers
+	**/
+
+	// Share example html template if not in Marketing
+	function shouldShareExample (state) {
+		return (state === 'Marketing')
+			? false
+			: true;
+	}
+
+	// Not Formatted -> notformatted
+	function toStateFormat (title) {
+		return title.replace(' ', '').toLowerCase();
+	}
+
+	// Create section folder (if doesn't exist)
+	// Create empty markdown / html templates
+	function scaffoldSubsection (path, tabs, shareExample) {			
+		mkdirp(path, function (err) {
+			if (err) { console.log(err); return; }
+			// Create shared html example template
+			if (shareExample) touch(path + '/example.html');
+
+			// Create markdown / html files for each tab
+			tabs.forEach(function(tab) {
+				tab = tab.toLowerCase();
+				if (tab != 'api')  touch(path + '/' + tab + '.md');
+				if (!shareExample) touch(path + '/' + tab + '.html');
+			});
+		});
+	}
+
 });
 
