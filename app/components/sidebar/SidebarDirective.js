08023af6a9968cd12d2b01496d104364828c4ade
Logan Bernard <bernard.logan4@gmail.com>
2015-08-17 10:44:32 -0400

    [sidebar] snug spacing on scroll

--- app/components/sidebar/SidebarDirective.js
+++ app/components/sidebar/SidebarDirective.js
@@ -5,7 +5,7 @@ var ComponentsModule = require('../_index');
 /**
  * @ngInject
  */
-function SidebarDirective() {
+function SidebarDirective($window) {
 	return {
 		restrict: 'E',
 		controller: 'SidebarCtrl',
@@ -13,8 +13,28 @@ function SidebarDirective() {
 		scope: {
 			routes: '='
 		},
-		templateUrl: 'components/sidebar/_sidebar.html'
+		templateUrl: 'components/sidebar/_sidebar.html',
+		link: linkFn
 	};
+
+	function linkFn () {
+		$(function () {
+			var sidebar = $('#sidebar');
+			var offset = sidebar.offset();
+			$window = $(window);
+
+			$window.scroll(function() {
+				if ($window.scrollTop() > offset.top) {
+					sidebar.stop().animate({
+						marginTop: $window.scrollTop() - offset.top
+					});
+				}
+				else {
+					sidebar.stop().animate({ marginTop: 0 });
+				}
+			});
+		})
+	}
 }
 
 ComponentsModule.directive('sidebar', SidebarDirective);
