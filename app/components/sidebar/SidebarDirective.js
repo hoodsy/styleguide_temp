29716d965eaa9f518e170ae284e7be1bb8d2aa9c
Logan Bernard <bernard.logan4@gmail.com>
2015-08-17 13:46:58 -0400

    [sidebar] scroll fix

--- app/components/sidebar/SidebarDirective.js
+++ app/components/sidebar/SidebarDirective.js
@@ -5,7 +5,7 @@ var ComponentsModule = require('../_index');
 /**
  * @ngInject
  */
-function SidebarDirective($window) {
+function SidebarDirective($window, $timeout) {
 	return {
 		restrict: 'E',
 		controller: 'SidebarCtrl',
@@ -18,22 +18,25 @@ function SidebarDirective($window) {
 	};
 
 	function linkFn () {
-		$(function () {
-			var sidebar = $('#sidebar');
-			var offset = sidebar.offset();
-			$window = $(window);
+		// timeout used to give sidebar time to load on state change
+		$timeout(function () {
+			$(function () {
+				var sidebar = $('#sidebar');
+				var offset = sidebar.offset();
+				$window = $(window);
 
-			$window.scroll(function() {
-				if ($window.scrollTop() > offset.top) {
-					sidebar.stop().animate({
-						marginTop: $window.scrollTop() - offset.top
-					});
-				}
-				else {
-					sidebar.stop().animate({ marginTop: 0 });
-				}
+				$window.scroll(function() {
+					if ($window.scrollTop() > offset.top) {
+						sidebar.stop().animate({
+							marginTop: $window.scrollTop() - offset.top
+						});
+					}
+					else {
+						sidebar.stop().animate({ marginTop: 0 });
+					}
+				});
 			});
-		})
+		}, 1000);
 	}
 }
 
